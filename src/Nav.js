8441e1bce402683340da942f02dde84fb9f40d8e
Niimaii <nimaicgomes@gmail.com>
2023-02-12 12:43:33 -0300

    With Context API

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "./MovieContext";
 
 const Nav = () => {
+  const [movies, setMovies] = useContext(MovieContext);
   return (
     <nav>
       <h3>Dev Ed</h3>
-      <p>List of Movies: </p>
+      <p>List of Movies: {movies.length}</p>
     </nav>
   );
 };
